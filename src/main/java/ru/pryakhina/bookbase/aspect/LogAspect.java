1284462b8a394e8115b552a4ff5aed9aefef320a
Elena Pryakhina <elena@MacBook-Air-Elena.local>
2023-11-15 23:09:30 +0300

    change applicationContext, add LogAspect

--- src/main/java/ru/pryakhina/bookbase/aspect/LogAspect.java
+++ src/main/java/ru/pryakhina/bookbase/aspect/LogAspect.java
@@ -1,2 +1,25 @@
-package ru.pryakhina.bookbase.aspect;public class LogAspect {
+package ru.pryakhina.bookbase.aspect;
+
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class LogAspect {
+
+    @Around(value = "execution(* ru.pryakhina.bookbase.dao.*.*(..))")
+    public Object arroundAllRepositoryMethodsAdvice(
+            ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+        MethodSignature methodSignature = (MethodSignature)proceedingJoinPoint.getSignature();
+        String nameMethod = methodSignature.getName();
+
+        System.out.println("Begin of " + nameMethod);
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+        System.out.println("End of " + nameMethod);
+        return targetMethodResult;
+    }
 }
